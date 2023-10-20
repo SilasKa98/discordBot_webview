6ba693cd80d6c137767aa57fed566543d10b160c
SilasKa98 <silas.kammerer@googlemail.com>
2023-10-20 19:21:41 +0200

    added dataHandler class

---
+++ services/dataHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class DataHandler{
+
+    function inputToArrayFilter($input, $matchingTo){
+        $names = [];
+        foreach($input as $item){
+            array_push($names,$item[$matchingTo]);
+        }
+        return $names;
+    }
+
+    function inputToDictionaryFilter($input, $matchingBy, $matchingTo){
+        $dic = [];
+        foreach($input as $item){
+            $dic[$item[$matchingBy]] = $item[$matchingTo];
+        }
+        return $dic;
+    }
+
+
+    function getArrayKeyByValue($array, $keyword){
+        for($i=0;$i<count($array);$i++){
+            if($array[$i] == $keyword){
+                return $i;
+            }
+        }
+    }
+
+}
+
+
+
+?>
