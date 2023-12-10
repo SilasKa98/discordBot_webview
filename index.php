a5ac92e854b9e1ebd0a31cda037535b3a8990de0
SilasKa98 <silas.kammerer@googlemail.com>
2023-12-10 01:40:34 +0100

    changed index text

--- index.php
+++ index.php
@@ -157,7 +157,7 @@
                 <h1 class="display-5 fw-bold"><?php echo $actualBotServerCount;?> SERVERS ALREADY DECIDED TO USE OUR BOT</h1>
                 <div class="col-lg-6 mx-auto" style="width: 70%;">
                 <p class="lead mb-4">
-                    Join the Club and get the Bergfest Bot now!
+                    Add the Bot now to also enhance your server!
                 </p>
             </div>
         </div>
